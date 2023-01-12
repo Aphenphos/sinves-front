0cb51f7bec599e1229347f2feb4eaa45b2e703d0
Aphenphos <MaxwillWinters@gmail.com>
2023-01-12 00:05:57 -0400

    recieving and setting JWT cookie from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import type { Component } from "solid-js";
 
-import logo from "./logo.svg";
 import styles from "./App.module.css";
 import { getAllBusiness } from "./services/business";
+import { signIn } from "./services/auth";
 
 const App: Component = () => {
   async function getProjs() {
     await getAllBusiness();
   }
+  async function testLogin() {
+    await signIn("new", "password");
+  }
   return (
     <div class={styles.App}>
       <button onclick={getProjs}>Get</button>
+      <button onclick={testLogin}>Signin</button>
     </div>
   );
 };
---
+++ src/services/auth.ts
@@ -0,0 +1,21 @@
+const BASE_URL = "https://localhost:7185/api/";
+
+export async function signIn(username: string, password: string) {
+  const resp = await fetch(`${BASE_URL}user/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+    credentials: "include",
+  });
+  if (resp.ok) {
+    return resp.json();
+  } else {
+    return null;
+  }
+}
