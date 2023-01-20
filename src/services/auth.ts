cada02b4e51de40168f6bc6ac214b8f143779602
Aphenphos <MaxwillWinters@gmail.com>
2023-01-20 16:49:11 -0400

    error testing

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -39,6 +39,8 @@ export async function signUp(username: string, password: string) {
     const data = await resp.json();
     return data;
   } else {
+    await resp.json();
+    console.error(resp.json());
     return null;
   }
 }
